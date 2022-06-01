31f2aca2a889ca6eefc0d060285bd2d4fcb92f96
ghayas ali <team@logochemist.com>
2022-06-01 20:30:54 +0500

    Ghayas

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,8 @@ import Poll from 'mdi-material-ui/Poll'
 import CurrencyUsd from 'mdi-material-ui/CurrencyUsd'
 import HelpCircleOutline from 'mdi-material-ui/HelpCircleOutline'
 import BriefcaseVariantOutline from 'mdi-material-ui/BriefcaseVariantOutline'
-
 import CardStatisticsVerticalComponent from 'src/@core/components/card-statistics/card-stats-vertical'
-
 import ApexChartWrapper from 'src/@core/styles/libs/react-apexcharts'
-
-
 import Table from 'src/views/dashboard/Table'
 import Trophy from 'src/views/dashboard/Trophy'
 import TotalEarning from 'src/views/dashboard/TotalEarning'
